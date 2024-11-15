609b0e8a3bc772edbb3e8d3c08336f559b41a1f1
gtmopyar12 <gtmrockstargaming@gmail.com>
2024-11-15 14:14:54 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+# Copyright (C) @TheSmartBisnu
+# Channel: https://t.me/itsSmartDev
+
+# Pyrogram setup
+API_ID = "23354483"  # Replace this API ID with your actual API ID
+API_HASH = "f71a7d38e09ef186f5ed539639ba1e27"  # Replace this API HASH with your actual API HASH
+SESSION_STRING = "BQFkXHMAU4CUG_Xs8dYKNtxsS429VJxxflwMSOTpI-gzCGO5Zw3vh7T49S0CQD2w7Cn6-qHypm57vN6VTi4Hf2A5aLIEaGfUt7Z9pvojhz_8ZwAhJ98Bb6oNdSufuwz5hkkZCdECbO8BBvhc4-E0G3ndE_5KU3S8f_VeGxE9Ll8ca9V83CCme-NWuITC1bFIR1e73RZQq1EFbQq9C2jt-MwFnDeLTJVnsscrfV68uyz4loP2McGguX4y35qVt7FRK-QJ4GknehkCXi_cFfBkJ2XI6WWPDwAn24WXAr-gejW6JamasnNRtf7Rw7aYlLZrLLvwR2pF7362s9-Yo5R905z4SGJZEAAAAAG3yaWwAA"  # Replace this SESSION STRING with your actual SESSION_STRING
+BOT_TOKEN = "7766894443:AAHNaiLx6x0TCbt__yH_U4bTdmVbuDkI-bg"  # Replace this BOT_TOKEN
+
+# Admin IDs
+ADMIN_IDS = [7378412976, 6784844047]
+
+# Limits
+DEFAULT_LIMIT = 10000 # Card Scrapping Limit For Everyone
+ADMIN_LIMIT = 500000 # Card Scrapping Limit For Admin
